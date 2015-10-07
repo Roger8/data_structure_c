82aec3d5ad4b2ba1ce8ce0fb1d656d12d9a53b29
Roger8 <viomab@sina.com>
2015-10-07 11:03:49 +0800

    Update c++ 笔记.cpp

--- c++ 笔记.cpp
+++ c++ 笔记.cpp
@@ -18,12 +18,17 @@ int * const * - a pointer to a const pointer to an int
 int const ** - a pointer to a pointer to a const int
 int * const * const - a const pointer to a const pointer to an int
 /*
-split the statement at asterix sign, then, if the const keyword appears in the left part (like in 'const int * foo') - it belongs to pointed data, if it's in the right part ('int * const bar') - it's about the pointer. 
+split the statement at asterix sign, then, if the const keyword appears 
+in the left part (like in 'const int * foo') - it belongs to pointed data,
+if it's in the right part ('int * const bar') - it's about the pointer. 
 */
 
 /*
 常量成员函数 
-声明带 const 关键字的成员函数将指定该函数是一个“只读”函数，它不会修改为其调用该函数的对象。 常量成员函数不能修改任何非静态数据成员或调用不是常量的任何成员函数。若要声明常量成员函数，请在参数列表的右括号后放置 const 关键字。 声明和定义中都需要 const 关键字。
+声明带 const 关键字的成员函数将指定该函数是一个“只读”函数，它不会修改为其
+调用该函数的对象。 常量成员函数不能修改任何非静态数据成员或调用不是常量的
+任何成员函数。若要声明常量成员函数，请在参数列表的右括号后放置 const 关键字。
+声明和定义中都需要 const 关键字。
 */
 // constant_member_function.cpp
 class Date
